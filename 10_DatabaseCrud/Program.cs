ab3d00c3bfa002abf468b825ca6dd4b3245d8b32
Derya Kalender <derya.sdev@gmail.com>
2024-11-14 16:58:11 +0300

    Database Crud Project added.

---
+++ 10_DatabaseCrud/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crud --> Create - Read - Update - Delete
+
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("------------------------");
+
+
+            #region Kategori Ekleme İşlemi
+
+            //Console.Write("Eklemek İstediğiniz Kategori Adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-FDKFGC2;initial Catalog=EgitimKampiDb;integrated security=true");
+
+            //sqlConnection.Open();
+
+            //SqlCommand cmd = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", sqlConnection);
+
+            //cmd.Parameters.AddWithValue("@p1", categoryName);
+            //cmd.ExecuteNonQuery();
+            //sqlConnection.Close();
+
+            //Console.WriteLine("Kategori başarıyla eklendi.");
+
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            //string productName;
+            //decimal productPrice;
+            //bool productStatus;
+
+            //Console.Write("Ürün Adı: ");
+            //productName = Console.ReadLine();
+            //Console.Write("Ürün Fiyatı: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-FDKFGC2;initial Catalog=EgitimKampiDb;integrated security=true");
+            //sqlConnection.Open();
+
+            //SqlCommand cmd = new SqlCommand("insert into TblProduct (ProductName, ProductPrice, ProductStatus) values (@productName, @productPrice, @productStatus)",sqlConnection);
+
+            //cmd.Parameters.AddWithValue("productName", productName);
+            //cmd.Parameters.AddWithValue("productPrice", productPrice);
+            //cmd.Parameters.AddWithValue("productStatus", true);
+            //cmd.ExecuteNonQuery();
+            //sqlConnection.Close();
+
+            //Console.WriteLine("Ürün eklemesi başarılı");
+
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+
+            //SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-FDKFGC2;initial Catalog=EgitimKampiDb;integrated security=true");
+
+            //sqlConnection.Open();
+            //SqlCommand cmd = new SqlCommand("Select * From TblProduct", sqlConnection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //sqlConnection.Close();
+
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach (var col in row.ItemArray)
+            //    {
+            //        Console.Write(col.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            //Console.Write("Silinecek Ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-FDKFGC2;initial Catalog=EgitimKampiDb;integrated security=true");
+            //sqlConnection.Open();
+            //SqlCommand cmd = new SqlCommand("Delete From TblProduct Where ProductId=@productId",sqlConnection);
+
+            //cmd.Parameters.AddWithValue("@productId",productId);
+            //cmd.ExecuteNonQuery();
+            //sqlConnection.Close();
+
+            //Console.WriteLine("Ürün başarıyla silindi.");
+            #endregion
+
+
+            #region Ürün Güncelleme İşlemi
+
+            Console.Write("Güncellenecek Ürün Id: ");
+            int productId = int.Parse(Console.ReadLine());
+
+            Console.Write("Güncellenecek Ürün Adı: ");
+            string productName = Console.ReadLine();
+
+            Console.Write("Güncellenecek Ürün Fiyatı: ");
+            decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-FDKFGC2;initial Catalog=EgitimKampiDb;integrated security=true");
+            sqlConnection.Open();
+
+            SqlCommand cmd = new SqlCommand("Update TblProduct Set ProductName=@productName, ProductPrice=@productPrice where ProductId=@productId ", sqlConnection);
+            cmd.Parameters.AddWithValue("@productName", productName);
+            cmd.Parameters.AddWithValue("@productPrice", productPrice);
+            cmd.Parameters.AddWithValue("@productId", productId);
+            cmd.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            Console.WriteLine("Ürün başarıyla güncellendi.");
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,36 +2,39 @@
 ## Bu repository Murat Yücedağ'ın Youtube üzerinde verdiği .Net derslerindeki practise projelerini içermektedir.
 
 # 101 Modülü
-### Bu modülde C# temel konularını öğrenip örnek uygulamalarla bilgilerimizi kullanacağımız projeler yaptık.
+#### Bu modülde C# temel konularını öğrenip örnek uygulamalarla bilgilerimizi kullanacağımız projeler yaptık.
 
 ---
 ## ✅ 01_MainSubjects Projesi
-### Bu projede C# temel yapısına değinip değişkenler konusuna giriş yaptık. Console fonsksiyonları ile yazdırma, okuma işlemini pratiğe döktük.
+#### Bu projede C# temel yapısına değinip değişkenler konusuna giriş yaptık. Console fonsksiyonları ile yazdırma, okuma işlemini pratiğe döktük.
 
 ## ✅ 02_Variables Projesi
-### Bu projede C# değişkenler konusunu örnekler ile birlikte işledik. Console fonsksiyonu ile kullanıcıdan aldığımız verileri değişkenler arası dönüşümlerini gerçekleştirdik.
+#### Bu projede C# değişkenler konusunu örnekler ile birlikte işledik. Console fonsksiyonu ile kullanıcıdan aldığımız verileri değişkenler arası dönüşümlerini gerçekleştirdik.
 
 ## ✅ 03_MakingDecision Projesi
-### Bu projede karar yapılarını işledik. İf-Else ve Switch Case ile örnekler yaptık.
+#### Bu projede karar yapılarını işledik. İf-Else ve Switch Case ile örnekler yaptık.
 
 ## ✅ 04_Loops Projesi
-### Bu projede for - while döngülerini ve bu döngüler ile karar yapısı kullanımı öğrendik. Birden fazla kez yapılacak işlemleri programda otomatik haline getirecek uygulamalar yaptık.
+#### Bu projede for - while döngülerini ve bu döngüler ile karar yapısı kullanımı öğrendik. Birden fazla kez yapılacak işlemleri programda otomatik haline getirecek uygulamalar yaptık.
 
 ## ✅ 05_LoopsWithStars Projesi
-### Bu projede döngüleri kullanarak teknik testlerde en çok karşılaşabileceğimiz yıldız ile desen oluşturma örneklerini yaptık. Algoritma kurma becerimizi güçlendirdik.
+#### Bu projede döngüleri kullanarak teknik testlerde en çok karşılaşabileceğimiz yıldız ile desen oluşturma örneklerini yaptık. Algoritma kurma becerimizi güçlendirdik.
 
 ## ✅ 06_Arrays Projesi
-### Bu projede dizileri farklı değişken tiplerini kümeleyerek kullanımını ve dizi içerisindeki elemanlara nasıl ulaşabileceğimizi öğrendik. Birkaç dizi fonksiyonunu örnekler üzerinde deneyerek pratiğe döktük.
+#### Bu projede dizileri farklı değişken tiplerini kümeleyerek kullanımını ve dizi içerisindeki elemanlara nasıl ulaşabileceğimizi öğrendik. Birkaç dizi fonksiyonunu örnekler üzerinde deneyerek pratiğe döktük.
 
 ## ✅ 07_ForeachLoop Projesi
-### Bu projede foreach döngü yapısını ve diziler ile kullanımı öğrendik. Sınav sistemi uygulaması yazarak bilgilerimiz pekiştirdik.
+#### Bu projede foreach döngü yapısını ve diziler ile kullanımı öğrendik. Sınav sistemi uygulaması yazarak bilgilerimiz pekiştirdik.
 
 ## ✅ 08_Methods Projesi
-### Bu projede methodları ve method türlerini öğrendik. Geriye değer döndüren ve döndürmeyen methodları örneklerle inceledik. Sınav sonuçlarını değerlendiren bir programı string veri tipi döndüren bir method yazarak örnek üzerinde çalıştık.
+#### Bu projede methodları ve method türlerini öğrendik. Geriye değer döndüren ve döndürmeyen methodları örneklerle inceledik. Sınav sonuçlarını değerlendiren bir programı string veri tipi döndüren bir method yazarak örnek üzerinde çalıştık.
 
 # 201 Modülü
-###  Bu modülde C# orta seviye konularını öğrenip örnek uygulamalarla bilgilerimizi kullanacağımız projeler yaptık.
+####  Bu modülde C# orta seviye konularını öğrenip örnek uygulamalarla bilgilerimizi kullanacağımız projeler yaptık.
 
 ---
 ## ✅ 09_DatabaseProject Projesi
-### Bu projede ado.net ile database bağlantı yapısı kurarak verilerimizi program içerisinde okuma pratiği yaptık. MSSQL üzerinde DB kendimiz oluşturduk.
+#### Bu projede ado.net ile database bağlantı yapısı kurarak verilerimizi program içerisinde okuma pratiği yaptık. MSSQL üzerinde DB kendimiz oluşturduk.
+
+## ✅ 10_DatabaseCrud Projesi
+#### Bu projede ado.net ile kurduğumuz database bağlantı yapısı üzerinden Oluşturma-Okuma-Güncelleme-Silme Güncelleme(Create-Read-Update-Delete CRUD) işlemlerini gerçekleştirdik. Database üzerindeki verilerimizi koddan nasıl kontrol edebileceğimizi öğrendiik.
